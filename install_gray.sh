cd751e74cd1be6de748282899d055cb9736cfa89
Effective1212 <60982688+Effective1212@users.noreply.github.com>
2020-04-02 19:46:51 +0300

    Update install_gray.sh

--- install_gray.sh
+++ install_gray.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
 
-#graylog_pass=admin
-# http_ip=
-# td_agent_file=
+## Variables you should change
 
+graylog_pass=admin
+http_ip=192.168.67.141
+#td_agent_file=/root/td-agent.conf
 
-
-#tcp_ports= "80 443 8080"
+#tcp_ports= "80 9000 9200"
 #upd_ports=
 
-yum install epel-release -y 
-yum update -y 
-yum install net-tools  -y 
-yum install pwgen -y
+## Installation
 
-echo "[mongodb-org-4.0]
+yum install epel-release -y
+yum update -y
+yum install net-tools  -y
+yum install pwgen wget sudo -y
+yum install java-1.8.0-openjdk-headless.x86_64 -y
+yum install policycoreutils-python -y
+
+echo '
+[mongodb-org-4.2]
 name=MongoDB Repository
-baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.0/x86_64/
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
 gpgcheck=1
 enabled=1
-gpgkey=https://www.mongodb.org/static/pgp/server-4.0.asc
-" > /etc/yum.repos.d/mongodb-org.repo
+gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' > /etc/yum.repos.d/mongodb-org.repo
 
-yum install mongodb-org -y 
+
+yum install mongodb-org -y
 
 systemctl daemon-reload
 systemctl enable mongod.service
@@ -37,46 +42,42 @@ gpgcheck=1
 gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
 enabled=1
 autorefresh=1
-type=rpm-md" > /etc/yum.repos.d/elasticsearch.repo" > /etc/yum.repos.d/elasticsearch.repo
-yum install elasticsearch-oss -y 
-
-cp /etc/elasticsearch/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml.bak
-
-sed 's/^cluster\.name.*/cluster\.name graylog/' /etc/elasticsearch/elasticsearch.yml > /etc/elasticsearch/elasticsearch.yml
-
-
-echo "action.auto_create_index: false" >> /etc/elasticsearch/elasticsearch.yml 
+type=rpm-md" > /etc/yum.repos.d/elasticsearch.repo
+yum install elasticsearch-oss -y
 
 systemctl daemon-reload
 
-
-
 systemctl enable elasticsearch.service
+
+cp /etc/elasticsearch/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml.bak
+
+sed 's/^#cluster\.name.*/cluster\.name graylog/' /etc/elasticsearch/elasticsearch.yml.bak > /etc/elasticsearch/elasticsearch.yml
+
+echo "action.auto_create_index: false" >> /etc/elasticsearch/elasticsearch.yml
 
 systemctl restart elasticsearch.service
 
 rpm -Uvh https://packages.graylog2.org/repo/packages/graylog-3.2-repository_latest.rpm
 
-yum install -y  graylog-server 
-
-cp /etc/graylog/server/server.conf /etc/graylog/server/server.conf.bak
-
-gray_hash$(echo -n "$graylog_pass"|sha256sum | cut -d" " -f1)
-
-sed "s/^root_password_sha2.*/root_password_sha2=$gray_hash/" /etc/graylog/server/server.conf > /etc/graylog/server/server.conf
-
-pwgen_hash=$(pwgen -N 1 -s 96)
-
-sed "s/^.password_secret.*/password_secret=$pwgen_hash/" /etc/graylog/server/server.conf > /etc/graylog/server/server.conf
-
-
-
-
-sed "s/^http_bind_address.*/http_bind_address=$http_ip/" /etc/graylog/server/server.conf > /etc/graylog/server/server.conf
-
+yum install -y  graylog-server
 
 systemctl daemon-reload
 systemctl enable graylog-server.service
+
+cp /etc/graylog/server/server.conf /etc/graylog/server/server.conf.bak
+
+gray_hash=$(echo -n "$graylog_pass"|sha256sum | cut -d" " -f1)
+
+pwgen_hash=$(pwgen -N 1 -s 96)
+
+sed "s/^root_password_sha2.*/root_password_sha2=$gray_hash/" /etc/graylog/server/server.conf.bak > /etc/graylog/server/server.conf
+cp -f /etc/graylog/server/server.conf /etc/graylog/server/my.conf
+sed "s/^password_secret.*/password_secret=$pwgen_hash/" /etc/graylog/server/my.conf > /etc/graylog/server/server.conf
+cp -f /etc/graylog/server/server.conf /etc/graylog/server/my.conf
+sed "s/^#http_bind_address.*/http_bind_address=$http_ip:9000/" /etc/graylog/server/my.conf > /etc/graylog/server/server.conf
+rm -f /etc/graylog/server/my.conf
+
+
 systemctl start graylog-server.service
 
 
@@ -99,13 +100,16 @@ semanage port -a -t mongod_port_t -p tcp 27017
 ### Graylog Kurulumu Tamalandi ###
 
 
-curl -L https://toolbelt.treasuredata.com/sh/install-redhat-td-agent3.sh | sh
+curl -L https://toolbelt.treasuredata.com/sh/install-redhat-td-agent3.sh|sh
 
-systemctl daemon-reload 
+systemctl daemon-reload
 
 systemctl enable td-agent
 
+if [ ${tcp_ports} ]
+then
 cp $td_agent_file /etc/td-agent/td-agent.conf
+fi
 
 td-agent-gem install fluent-plugin-multi-format-parser
 td-agent-gem install fluent-plugin-gelf-hs
@@ -114,19 +118,27 @@ td-agent-gem install fluent-plugin-record-modifier --no-document
 
 ### Firewall Rules ##
 
-for i in $tcp_ports;
-do
-firewall-cmd --zone=public --add-port=$i/tcp --permanent;
-done
 
-for i in $udp_ports;
+if [ ${tcp_ports} ]
+then
+for i in $tcp_ports
 do
-firewall-cmd --zone=public --add-port=$i/upd --permanent;
+firewall-cmd --zone=public --add-port=$i/tcp --permanent
 done
+fi
+
+
+
+if [ ${udp_ports} ]
+then
+for i in $udp_ports
+do
+firewall-cmd --zone=public --add-port=$i/upd --permanent
+done
+fi
 
 firewall-cmd --reload
 
 systemctl restart td-agent
 
 echo "DONE"
-
