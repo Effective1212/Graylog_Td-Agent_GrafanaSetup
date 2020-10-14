787c701047b470fa17fa7f47be60cbb4764c78d5
Effective1212 <60982688+Effective1212@users.noreply.github.com>
2020-10-14 11:25:00 +0300

    Update install_gray.sh

--- install_gray.sh
+++ install_gray.sh
@@ -1,22 +1,64 @@
 #!/bin/bash
 
-## Variables you should change
+############################ Declare Variables ############################
 
-graylog_pass=admin
-http_ip=192.168.67.141
-#td_agent_file=/root/td-agent.conf
+graylog_pass=kOa9aPmW
+http_ip=192.168.48.180
+td_agent_file="/root/td-agent.conf"
 
-#tcp_ports= "80 9000 9200"
-#upd_ports=
+td_agent_plugins="/root/plugin/custom-plugins/"
 
-## Installation
 
-yum install epel-release -y
+tcp_ports=(80 9000 3000 11514 514 443 )
+upd_ports=(601 514)
+
+
+############################ STARTING INSTLATION ############################
+
+
 yum update -y
+yum install epel-release -y
 yum install net-tools  -y
 yum install pwgen wget sudo -y
 yum install java-1.8.0-openjdk-headless.x86_64 -y
 yum install policycoreutils-python -y
+
+
+setenforce Permissive
+
+echo -e "\e[0;44mUpdates Are Finissed!!!\e[0m"
+
+
+###### Grafana Installation
+
+
+echo -e "\e[0;44mStarting Installation of Grafana-Server\e[0m"
+
+echo '[grafana]
+name=grafana
+baseurl=https://packages.grafana.com/oss/rpm
+repo_gpgcheck=1
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.grafana.com/gpg.key
+sslverify=1
+sslcacert=/etc/pki/tls/certs/ca-bundle.crt' > /etc/yum.repos.d/grafana.repo
+
+
+yum install grafana -y 
+
+sudo systemctl daemon-reload
+systemctl enable grafana-server
+sudo systemctl start grafana-server
+
+
+
+asd=$(systemctl --type=service --state=active | grep grafana)
+echo -e "\e[0;44mGrafana-Server is\e[0m \e[0;43m$asd\e[0m"
+
+
+
+echo -e "\e[e0;44mStarting Installation of MongoDB\e[0m"
 
 echo '
 [mongodb-org-4.2]
@@ -32,7 +74,13 @@ yum install mongodb-org -y
 systemctl daemon-reload
 systemctl enable mongod.service
 systemctl start mongod.service
-systemctl --type=service --state=active | grep mongod
+
+asd=$(systemctl --type=service --state=active | grep mongod)
+
+echo -e "\e[0;44mMongoDB is\e[0m \e[0;43m$asd\e[0m"
+
+echo -e "\e[0;44mStarting Installation of elasticsearch\e[0m"
+
 rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
 
 echo "[elasticsearch-6.x]
@@ -43,6 +91,7 @@ gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
 enabled=1
 autorefresh=1
 type=rpm-md" > /etc/yum.repos.d/elasticsearch.repo
+
 yum install elasticsearch-oss -y
 
 systemctl daemon-reload
@@ -56,6 +105,12 @@ sed 's/^#cluster\.name.*/cluster\.name graylog/' /etc/elasticsearch/elasticsearc
 echo "action.auto_create_index: false" >> /etc/elasticsearch/elasticsearch.yml
 
 systemctl restart elasticsearch.service
+
+asd=$(systemctl --type=service --state=active | grep elasticsearch)
+
+echo -e "\e[0;44mElasticsearch is \e[0m \e[0;43m$asd\e[0m"
+
+echo -e "\e[0;44mStarting Installation of Graylog-Server\e[0m"
 
 rpm -Uvh https://packages.graylog2.org/repo/packages/graylog-3.2-repository_latest.rpm
 
@@ -77,8 +132,14 @@ cp -f /etc/graylog/server/server.conf /etc/graylog/server/my.conf
 sed "s/^#http_bind_address.*/http_bind_address=$http_ip:9000/" /etc/graylog/server/my.conf > /etc/graylog/server/server.conf
 rm -f /etc/graylog/server/my.conf
 
+systemctl daemon-reload
 
 systemctl start graylog-server.service
+
+
+asd=$(systemctl --type=service --state=active | grep graylog)
+
+echo -e "\e[0;44mGraylog-Server is \e[0m \e[0;43m$asd\e[0m"
 
 
 #Firewall Rules
@@ -92,13 +153,11 @@ setsebool -P httpd_can_network_connect 1
 
 semanage port -a -t http_port_t -p tcp 9000
 
-semanage port -a -t http_port_t -p tcp 9200
-
-semanage port -a -t mongod_port_t -p tcp 27017
+semanage port -a -t mongod_port_t -p tcp 3000
 
 
-### Graylog Kurulumu Tamalandi ###
 
+echo -e "\e[0;44mStarting Installation of Td-Agent\e[0m"
 
 curl -L https://toolbelt.treasuredata.com/sh/install-redhat-td-agent3.sh|sh
 
@@ -111,34 +170,58 @@ then
 cp $td_agent_file /etc/td-agent/td-agent.conf
 fi
 
+echo -e "\e[0;44mTd-Agent Gem plugins are installing\e[0m"
+
 td-agent-gem install fluent-plugin-multi-format-parser
 td-agent-gem install fluent-plugin-gelf-hs
 td-agent-gem install fluent-plugin-record-modifier --no-document
 
+echo -e "\e[0;44mDone with Td-Agent Gem plugins installation\e[0m"
+
+mkdir '/etc/td-agent/custom-plugins'
+
+cp $td_agent_plugins /etc/td-agent/custom-plugins/
+
+cp /etc/systemd/system/multi-user.target.wants/td-agent.service /root/td-agent.service 
+
+sed "s/User=.*/User=root/ " /root/td-agent.service > /etc/systemd/system/multi-user.target.wants/td-agent.service
+
+sed "s/Group=.*/Group=root/" /root/td-agent.service > /etc/systemd/system/multi-user.target.wants/td-agent.service
+
+systemctl daemon-reload
+
+systemctl restart td-agent
+asd=$(systemctl --type=service --state=active | grep td-agent)
+
+echo -e "\e[0;44mTd-Agent is \e[0m \e[0;43m$asd\e[0m"
+
 
 ### Firewall Rules ##
+
+echo -e "\e[0;44mFirewalld rules are starting to generate\e[0m"
 
 
 if [ ${tcp_ports} ]
 then
-for i in $tcp_ports
+for i in ${tcp_ports[@]}
 do
 firewall-cmd --zone=public --add-port=$i/tcp --permanent
 done
 fi
 
 
-
 if [ ${udp_ports} ]
 then
-for i in $udp_ports
+for i in ${udp_ports[@]}
 do
-firewall-cmd --zone=public --add-port=$i/upd --permanent
+firewall-cmd --zone=public --add-port=$i/udp --permanent
 done
 fi
 
+
+systemctl daemon-reload
+
 firewall-cmd --reload
 
-systemctl restart td-agent
 
-echo "DONE"
+echo -e "\e[0;43mDONE!!!!\e[0m"
